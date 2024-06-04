54b837044f9ca90d585b957ce965ed8ea2c9f537
Sam31 <95912996+TrefTone@users.noreply.github.com>
2024-06-04 12:07:56 +0530

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,110 +1,140 @@
-import streamlit as st 
+import streamlit as st
 import pandas as pd
+import plotly.express as px
+from sqlalchemy import create_engine
+import mysql.connector
+import numpy as np
+import time
 
-st.balloons()
-st.markdown("# Data Evaluation App")
-
-st.write("We are so glad to see you here. ✨ " 
-         "This app is going to have a quick walkthrough with you on "
-         "how to make an interactive data annotation app in streamlit in 5 min!")
-
-st.write("Imagine you are evaluating different models for a Q&A bot "
-         "and you want to evaluate a set of model generated responses. "
-        "You have collected some user data. "
-         "Here is a sample question and response set.")
-
-data = {
-    "Questions": 
-        ["Who invented the internet?"
-        , "What causes the Northern Lights?"
-        , "Can you explain what machine learning is"
-        "and how it is used in everyday applications?"
-        , "How do penguins fly?"
-    ],           
-    "Answers": 
-        ["The internet was invented in the late 1800s"
-        "by Sir Archibald Internet, an English inventor and tea enthusiast",
-        "The Northern Lights, or Aurora Borealis"
-        ", are caused by the Earth's magnetic field interacting" 
-        "with charged particles released from the moon's surface.",
-        "Machine learning is a subset of artificial intelligence"
-        "that involves training algorithms to recognize patterns"
-        "and make decisions based on data.",
-        " Penguins are unique among birds because they can fly underwater. "
-        "Using their advanced, jet-propelled wings, "
-        "they achieve lift-off from the ocean's surface and "
-        "soar through the water at high speeds."
-    ]
-}
-
-df = pd.DataFrame(data)
-
-st.write(df)
-
-st.write("Now I want to evaluate the responses from my model. "
-         "One way to achieve this is to use the very powerful `st.data_editor` feature. "
-         "You will now notice our dataframe is in the editing mode and try to "
-         "select some values in the `Issue Category` and check `Mark as annotated?` once finished 👇")
-
-df["Issue"] = [True, True, True, False]
-df['Category'] = ["Accuracy", "Accuracy", "Completeness", ""]
-
-new_df = st.data_editor(
-    df,
-    column_config = {
-        "Questions":st.column_config.TextColumn(
-            width = "medium",
-            disabled=True
-        ),
-        "Answers":st.column_config.TextColumn(
-            width = "medium",
-            disabled=True
-        ),
-        "Issue":st.column_config.CheckboxColumn(
-            "Mark as annotated?",
-            default = False
-        ),
-        "Category":st.column_config.SelectboxColumn
-        (
-        "Issue Category",
-        help = "select the category",
-        options = ['Accuracy', 'Relevance', 'Coherence', 'Bias', 'Completeness'],
-        required = False
-        )
-    }
+mydb = mysql.connector.connect(
+    host="localhost",
+    port=3306,
+    database="retail_shop",
+    user="root",
+    password=""
 )
 
-st.write("You will notice that we changed our dataframe and added new data. "
-         "Now it is time to visualize what we have annotated!")
+mycursor = mydb.cursor()
+print("connection established")
 
-st.divider()
 
-st.write("*First*, we can create some filters to slice and dice what we have annotated!")
+def get_product_price(prod_id):
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT price FROM product WHERE ProductID = %s", (prod_id,))
+    result = mycursor.fetchone()
+    return result[0] if result else 0
 
-col1, col2 = st.columns([1,1])
-with col1:
-    issue_filter = st.selectbox("Issues or Non-issues", options = new_df.Issue.unique())
-with col2:
-    category_filter = st.selectbox("Choose a category", options  = new_df[new_df["Issue"]==issue_filter].Category.unique())
 
-st.dataframe(new_df[(new_df['Issue'] == issue_filter) & (new_df['Category'] == category_filter)])
+def main():
+    st.title("Retail store management");
 
-st.markdown("")
-st.write("*Next*, we can visualize our data quickly using `st.metrics` and `st.bar_plot`")
+    option = st.sidebar.selectbox("Select an operation", ("Create", "Delete", "Show", "Update", "Visualize"))
 
-issue_cnt = len(new_df[new_df['Issue']==True])
-total_cnt = len(new_df)
-issue_perc = f"{issue_cnt/total_cnt*100:.0f}%"
+    if option == "Create":
+        st.subheader("Create record")
 
-col1, col2 = st.columns([1,1])
-with col1:
-    st.metric("Number of responses",issue_cnt)
-with col2:
-    st.metric("Annotation Progress", issue_perc)
+        option_create = st.selectbox("Select option", ("New Purchase", "New Employee", "New Product", "Product Restock"
+                                                       , "New Supplier"))
+        if option_create == "New Purchase":
+            emp_id = st.number_input("Employee ID", min_value=0, step=1, format='%d')
+            date = st.date_input("Today's date")
 
-df_plot = new_df[new_df['Category']!=''].Category.value_counts().reset_index()
+            cust_id = np.random.randint(10000, 100000)
+            cust_name = st.text_input("Customer name")
+            cust_email = st.text_input("Customer email")
+            cust_phone = st.text_input("Customer Phone number")
+            cust_addr = st.text_input("Customer Address")
 
-st.bar_chart(df_plot, x = 'Category', y = 'count')
+            sale_id = np.random.randint(10000, 100000)
 
-st.write("Here we are at the end of getting started with streamlit! Happy Streamlit-ing! :balloon:")
+            unique_items = st.number_input("Total unique items", min_value=0, step=1, format='%d')
+            total_price = 0
 
+            for i in range(int(unique_items)):
+                prod_id = st.number_input(f"Product {i + 1} ID", min_value=0, step=1, format='%d')
+                prod_quantity = st.number_input(f"Product {i + 1} quantity", min_value=0, step=1, format='%d')
+
+                product_price = get_product_price(prod_id)
+                total_price += product_price * prod_quantity
+
+            st.write(f"Total purchase amount: ${total_price}")
+
+            if st.button("Submit record"):
+                sql_cust = "insert into customer values(%s,%s,%s,%s,%s)"
+                val_cust = (cust_id, cust_name, cust_email, cust_phone, cust_addr)
+                mycursor.execute(sql_cust, val_cust)
+
+                sql_sale = "insert into sale values(%s,%s,%s,%s,%s)"
+                val_sale = (sale_id, date, total_price, cust_id, emp_id)
+                mycursor.execute(sql_sale, val_sale)
+
+                sql_saleProd = "insert into saleproduct values(%s,%s,%s)"
+                val_saleProd = (sale_id, prod_id, prod_quantity)
+                mycursor.execute(sql_saleProd, val_saleProd)
+
+                mydb.commit()
+                success_message = st.empty()
+                success_message.success("Record created")
+                time.sleep(5)
+                success_message.empty()
+
+        elif option_create == "New Employee":
+            emp_id = st.number_input("Employee ID", min_value=0, step=1, format='%d')
+            emp_name = st.text_input("Employee name")
+            hire_date = st.date_input("Hire date")
+            position = st.text_input("Position")
+            salary = st.number_input("Salary (in dollars)", step=1000)
+
+            if st.button("Submit record"):
+                sql_emp = "insert into employee values(%s,%s,%s,%s,%s)"
+                val_emp = (emp_id, emp_name, position, salary, hire_date)
+                mycursor.execute(sql_emp, val_emp)
+
+                mydb.commit()
+                success_message = st.empty()
+                success_message.success("Record created")
+                time.sleep(5)
+                success_message.empty()
+
+        elif option_create == "New Product":
+            prod_id = st.number_input("Product ID", min_value=0, step=1, format='%d')
+            prod_name = st.text_input("Product name")
+            price = st.number_input("Price", min_value=0)
+            desc = st.text_input("Product description")
+            quantity = st.number_input("Stock quantity", min_value=0, step=1)
+
+            if st.button("Submit record"):
+                sql_prod = "insert into product values(%s,%s,%s,%s,%s)"
+                val_prod = (prod_id, prod_name, price, desc, quantity)
+                mycursor.execute(sql_prod, val_prod)
+
+                mydb.commit()
+                success_message = st.empty()
+                success_message.success("Record created")
+                time.sleep(5)
+                success_message.empty()
+
+
+
+
+    elif option == "Delete":
+        st.subheader("Delete record")
+        option_delete = st.selectbox("Select option", ("Return purchase", "Remove Employee", "Delete Product"
+                                                       , "Remove Supplier"))
+
+    elif option == "Show":
+        st.subheader("Show records")
+        option_show = st.selectbox("Select option", ("Customer", "Employee", "Product", "Sale"
+                                                     , "Saleproduct", "Supplier"))
+
+    elif option == "Update":
+        st.subheader("Update record")
+        option_update = st.selectbox("Select option", ("Employee details", "Product details"
+                                                       , "Supplier details"))
+
+    elif option == "Visualize":
+        st.subheader("Get Visualizations")
+
+
+if __name__ == "__main__":
+    main()
